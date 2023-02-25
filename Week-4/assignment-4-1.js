a2d1d44c905598ba84f83c944ab6c904584b416e
somyanath.mohanty <somyanath.mohanty@olx.com>
2023-02-25 16:47:00 +0530

    implemented catch handling

--- Week-4/assignment-4-1.js
+++ Week-4/assignment-4-1.js
@@ -6,48 +6,70 @@ const PROMISE_STATES = {
 
 function MyPromise (executor) {
   let status = PROMISE_STATES.PENDING,
-      // isResolved = false,
-      handlerChain = [],
-      value;
+      thenChain = [],
+      catchChain = [],
+      resolvedValue,
+      rejectedValue;
 
   function resolve (passedValue) {
-    value = passedValue;
+    resolvedValue = passedValue;
     status = PROMISE_STATES.FULFILLED;
 
-    if (handlerChain.length) {
-      handlerChain.reduce((accumulator, handleFunction) => handleFunction(accumulator), value);
+    if (thenChain.length) {
+      thenChain.reduce((accumulator, handleFunction) => handleFunction(accumulator), resolvedValue);
     }
-  }
+  };
+
+  function reject (error) {
+    rejectedValue = error;
+    status = PROMISE_STATES.REJECTED;
+
+    if (catchChain.length) {
+      catchChain.reduce((accumulator, catchFunction) => catchFunction(accumulator), rejectedValue);
+    }
+  };
 
   this.then = function (callback) {
-    handlerChain.push(callback.bind(this));
+    thenChain.push(callback.bind(this));
 
     if (status === PROMISE_STATES.FULFILLED) {
-      handlerChain.reduce((accumulator, handleFunction) => handleFunction(accumulator), value);
+      resolvedValue = thenChain.reduce((accumulator, handleFunction) => handleFunction(accumulator), resolvedValue);
+      thenChain.shift();
+    }
+
+    return this;
+  }
+
+  this.catch = function (callback) {
+    catchChain.push(callback.bind(this));
+
+    if (status === PROMISE_STATES.REJECTED) {
+      rejectedValue = catchChain.reduce((accumulator, catchFunction) => catchFunction(accumulator), rejectedValue);
+      catchChain = [];
     }
 
     return this;
   }
 
   try {
-    executor(resolve);
+    executor(resolve, reject);
   } catch (error) {
     throw new Error(error);
   }
 };
 
 function getNumber(delay) {
-  return new MyPromise((resolve) => {
+  return new MyPromise((resolve, reject) => {
     const randonNumber = Math.floor(Math.random() * 100) + 1;
     console.log(randonNumber);
   
     setTimeout(() => {
-      // if(randonNumber % 5 === 0) {
-        // reject(`Number ${randonNumber} Promise rejected after ${delay} miliseconds`);
-      // }
-      // else {
-        resolve(`Number ${randonNumber} Promise resolved after ${delay} miliseconds`)
-      // }
+      if(randonNumber % 5 === 0) {
+        reject(`Number ${randonNumber} Promise rejected after ${delay} miliseconds`);
+      }
+      else {
+        reject(`Number ${randonNumber} Promise resolved after ${delay} miliseconds`)
+      }
     }, 1000)
   })
 };
@@ -60,3 +82,13 @@ getNumber(1000)
   .then((response) => {
     console.log(`inside the next then ${response}`);
   })
+  .catch((error) => {
+    console.log(`Error is here: ${error}`);
+    return error;
+  })
+  .catch((error) => {
+    console.log(`next catch ${error}`);
+  })
+  // .then((response) => {
+  //   console.log(`stupidity ${response}`);
+  // })
